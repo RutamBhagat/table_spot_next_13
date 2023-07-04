f7d0d7424824b400af970bd4b8599ada23da3286
RutamBhagat <rutambhagat@gmail.com>
2023-07-04 15:17:32 +0530

    add session verification at reservation

--- app/api/restaurant/[slug]/availability/route.ts
+++ app/api/restaurant/[slug]/availability/route.ts
@@ -1,8 +1,16 @@
 import { findAvailableTables } from "@/services/restaurant/findAvailableTables";
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 export async function GET(request: Request, { params }: { params: { slug: string } }) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
   const { searchParams } = new URL(request.url);
   const slug = params.slug;
   const day = searchParams.get("day");
--- app/api/restaurant/[slug]/reserve/route.ts
+++ app/api/restaurant/[slug]/reserve/route.ts
@@ -1,8 +1,16 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { findAvailableTables } from "@/services/restaurant/findAvailableTables";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 export async function POST(request: Request, { params }: { params: { slug: string } }) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
   const req = await request.json(); // req now is the body
   const { bookerEmail, bookerPhone, bookerName, bookerOccasion, bookerRequest } = req;
 
