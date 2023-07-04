5f0959fe3d49af91ee34caf23316835e8cf747e2
RutamBhagat <rutambhagat@gmail.com>
2023-07-04 16:01:01 +0530

    update env variable

--- app/(Pages Group)/restaurant/[slug]/error.tsx
+++ app/(Pages Group)/restaurant/[slug]/error.tsx
@@ -5,7 +5,7 @@ export default function error({ error }: { error: Error }) {
   return (
     <div className="flex justify-center items-center w-full min-h-screen">
       <div role="status">
-        <span className="font-semibold text-lg text-gray-700 ">{error.message}</span>
+        <span className="font-semibold text-lg text-gray-700">{error.message}</span>
       </div>
     </div>
   );
